4c8c33dacadb2fe6c879504b1accd5388a63967b
tegbiye <se.tegbabu.solomon@gmail.com>
2024-09-19 22:19:49 +0300

    Updated the auth.py

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -69,3 +69,19 @@ class Auth:
                 The user
         """
         return request
+    
+
+    def session_cookie(self, request=None):
+        """Returns a cookie value from a request"""
+
+        if request is None:
+            return None
+
+        SESSION_NAME = getenv("SESSION_NAME")
+
+        if SESSION_NAME is None:
+            return None
+
+        session_id = request.cookies.get(SESSION_NAME)
+
+        return session_id
